8010e30d843d9983e54662fdba09d4b609850e9b
dillonzellis <dillonzellis@gmail.com>
2025-05-25 10:50:22 -0400

    getting started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,69 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const exp = [
+    {
+      company: "Cox Enterprises(Atlanta Journal Constitution)",
+      time: "2024 - Present",
+      role: "Software Developer",
+      desc: "5+ million page views",
+    },
+    {
+      company: "League of SouthEastern Credit Unions",
+      time: "2022 - 2024",
+      role: "Software Developer",
+      desc: "marketing",
+    },
+    {
+      company: "RLM Affairs",
+      time: "2017 - 2021",
+      role: "Operations and Web Developer",
+      desc: "Event Space",
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+  return (
+    <div className="space-y-4 pt-12">
+      <h1 className="">
+        <span className="font-medium">Dillon Ellis</span> |{" "}
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="text-blue-500 hover:text-blue-700 dark:text-gray-400 hover:dark:text-gray-300 dark:underline dark:underline-offset-2 dark:decoration-gray-800"
+          href="https://github.com/Dillonzellis"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          @dillonzellis
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </h1>
+
+      <p className="leading-snug">
+        Full-stack software developer with 7+ years of experience building
+        scalable web applications and leading technical initiatives. Expertise
+        in React ecosystem, modern JavaScript frameworks, and cloud architecture
+        with a focus on performance optimization and user experience.
+      </p>
+
+      <p className="leading-snug">
+        Currently developing high-performance systems at Cox Enterprises with
+        expertise in React, CMS platforms, and modern web technologies.
+      </p>
+
+      <div>
+        <h2 className="font-medium">Experience</h2>
+        <hr />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        {exp.map((exp, idx) => (
+          <div key={idx} className="pb-4">
+            <div className="flex justify-between">
+              <div>{exp.company}</div>
+              <div className="text-sm text-gray-500">{exp.time}</div>
+            </div>
+            <div className="text-sm text-gray-700">{exp.role}</div>
+            <p className="pt-2 text-sm leading-snug text-gray-700">
+              {exp.desc}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
